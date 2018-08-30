5e0af3e1a1216f3595051a6c2d54b4e4b630046a
Frank <fatukunda@gmail.com>
2018-08-30 18:16:29 +0300

    Deposit method completed

--- bank.py
+++ bank.py
@@ -2,6 +2,7 @@ from account import Account
 from client import Client
 class Bank:
     allAccounts = []
+    # create an account which creates a new client and saves their information in a dictionary
     def createAccount(self, accountNumber, firstName, lastName):
         account = Account(accountNumber)
         account.client.firstName = firstName
@@ -9,7 +10,7 @@ class Bank:
         account.client.accounts.append(account)
         newAccount = { "Account Number": accountNumber, "First Name": firstName, "Last Name": lastName, "Balance": account.balance}
         self.allAccounts.append(newAccount)
-
+# Print all the accounts in the bank
     def displayAllAccounts(self):
          print('Accounts in the Bank')
          print('-------------------------------------')
@@ -19,7 +20,7 @@ class Bank:
             print('-------------------------------------')
             for key in account:
                 print('%s : %s' % (key, account[key]))
-
+# Search for a specific account given the account Number
     def searchAccount(self, accountNumber):
         for account in self.allAccounts:
             for clientAccount in account:
@@ -29,10 +30,21 @@ class Bank:
                      print('%s: %s' % (clientAccount, account[clientAccount]))
                      print('First Name: %s' % (account['First Name']))
                      print('Last Name: %s' % (account['Last Name']))
-            
+                     print('Balance: %d' % (account['Balance']))
+# deposit money
+    def deposit(self, accountNumber, amount):
+        for account in self.allAccounts:
+            for clientAccount in account:
+                if account[clientAccount] == accountNumber:
+                    account['Balance'] += amount
 
 myBank = Bank()
 myBank.createAccount(444444444, 'Frank', 'Atukunda')
 myBank.createAccount(555555555, 'Simon', 'Lee')
 #myBank.displayAllAccounts()
 myBank.searchAccount(555555555)
+myBank.deposit(555555555, 500)
+myBank.searchAccount(555555555)
+myBank.deposit(555555555, 500)
+myBank.searchAccount(555555555)
+
